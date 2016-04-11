001488d526ddd4e61f304c663b9cac011bbf43b7
Marco Medrano <marco.medrano.garay@gmail.com>
2016-04-11 10:28:20 -0400

    Added support for installation and update.

---
+++ all/Installation/Actions.cs
@@ -0,0 +1,31 @@
+﻿namespace All
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    class Actions
+    {
+        internal void SetOsPath()
+        {
+            var paths = Environment.GetEnvironmentVariable("PATH");
+            var currentDirectory = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory;
+            string parentDirectory = currentDirectory.Parent.FullName;
+
+            var pathsList = paths.Split(';').ToList();
+            var oldPath = pathsList.FirstOrDefault(path => path.StartsWith(parentDirectory, StringComparison.InvariantCultureIgnoreCase));
+
+            if (!string.IsNullOrEmpty(oldPath))
+            {
+                pathsList.Remove(oldPath);
+            }
+
+            pathsList.Add(currentDirectory.FullName);
+            paths = string.Empty;
+            pathsList.ForEach(p => paths += p + ";");
+
+            Environment.SetEnvironmentVariable("PATH", paths, EnvironmentVariableTarget.User);
+        }
+    }
+}
---
+++ all/Installation/Installation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.Installation
+{
+    using Squirrel;
+
+    class Installation
+    {
+        private readonly Actions actions = new Actions();
+        internal void OnInstallationAndUpdate()
+        {
+            using (var mgr = new UpdateManager(@"c:\lr.m\all\Releases\"))
+            {
+                // Note, in most of these scenarios, the app exits after this method completes!
+                SquirrelAwareApp.HandleEvents(
+                  onInitialInstall: v => this.actions.SetOsPath(),
+                  onAppUpdate: v => this.actions.SetOsPath(),
+                  onAppUninstall: v => { },
+                  onFirstRun: () => { });
+            }
+        }
+
+        internal void Update()
+        {
+            Console.WriteLine("Updating ...");
+            using (var mgr = new UpdateManager(@"c:\lr.m\all\Releases\"))
+            {
+                Task<ReleaseEntry> updateApp = mgr.UpdateApp();
+                var res = updateApp.Result;
+
+                if (res != null)
+                    Console.WriteLine("Updated to " + res.Version);
+                else
+                    Console.WriteLine("No new versions");
+            }
+        }
+    }
+}
